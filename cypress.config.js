061ca765b986781bf415bb6900c75dd31105dcbd
Ilya Tyupo <derherrmannelig@gmail.com>
2023-11-08 20:56:55 +0200

    feat: setup cypress cloud report

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 console.log("Current configuration: default.");
 
 module.exports = defineConfig({
+  projectId: '4ahx8w',
   e2e: {
     baseUrl: 'https://demo.realworld.io/#/',
   },
--- mobile.config.js
+++ mobile.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 console.log("Current configuration: mobile.");
 
 module.exports = defineConfig({
+  projectId: '4ahx8w',
   userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3',
   viewportHeight: 568,
   viewportWidth: 320,
